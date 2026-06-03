eadee571905d5b3f662de2e1df73213338864626
Bruno Bispo dos Santos <brunobispof2p@gmail.com>
2026-06-03 14:03:49 -0300

    Update base path for production environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess({ postcss: true }),
+
   kit: {
     adapter: adapter({
       fallback: '200.html'
     }),
+
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/scopus-base-manager' : ''
+      base: process.env.NODE_ENV === 'production'
+        ? '/Artigos_Florestais'
+        : ''
     }
   }
 };
